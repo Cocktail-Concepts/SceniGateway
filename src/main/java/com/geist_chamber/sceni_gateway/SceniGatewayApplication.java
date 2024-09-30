bbe3dbcac5b476b8d1329e6f48c18c0736f781e4
geistUto <geistuto@gmail.com>
2024-09-30 18:51:25 +0530

    gateway cors config

--- src/main/java/com/geist_chamber/sceni_gateway/SceniGatewayApplication.java
+++ src/main/java/com/geist_chamber/sceni_gateway/SceniGatewayApplication.java
@@ -2,14 +2,12 @@ package com.geist_chamber.sceni_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SceniGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SceniGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SceniGatewayApplication.class, args);
+    }
 
 }
--- src/main/java/com/geist_chamber/sceni_gateway/config/RouteLocator.java
+++ src/main/java/com/geist_chamber/sceni_gateway/config/RouteLocator.java
@@ -1,14 +1,22 @@
 package com.geist_chamber.sceni_gateway.config;
 
 import com.geist_chamber.sceni_gateway.security.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration(proxyBeanMethods = false)
+@EnableDiscoveryClient
 public class RouteLocator {
     private final AuthenticationFilter filter;
+    @Value("${ec2uri}")
+    private String ec2uri;
 
     public RouteLocator(AuthenticationFilter filter) {
         this.filter = filter;
@@ -19,11 +27,27 @@ public class RouteLocator {
         return builder.routes()
                 .route("GEIST_SERVICE", r -> r.path("/api/v1/geists/**")
                         .filters(f -> f.filter(filter))
-                        .uri("http://localhost:8081"))
-
-                .route("auth-service", r -> r.path("/auth/**")
+                        .uri(ec2uri+":8081"))
+                .route("MIND_SNIPPET_SERVICE", r -> r.path("/api/v1/mind-snippets/**")
                         .filters(f -> f.filter(filter))
-                        .uri("lb://auth-service"))
+                        .uri(ec2uri+":8083"))
                 .build();
+    }
+//    @Bean
+//    public org.springframework.cloud.gateway.route.RouteLocator routes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("GEIST_SERVICE", r -> r.path("/api/v1/geists/**")
+//                        .filters(f -> f.filter(filter))
+//                        .uri("lb://GEIST_SERVICE"))  // Use Eureka discovery
+//                .route("MIND_SNIPPET_SERVICE", r -> r.path("/api/v1/mind-snippets/**")
+//                        .filters(f -> f.filter(filter))
+//                        .uri("lb://MIND_SNIPPET_SERVICE"))  // Use Eureka discovery
+//                .build();
+//    }
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+            ReactiveDiscoveryClient discoveryClient, DiscoveryLocatorProperties properties) {
+        properties.setEnabled(true);  // Enabling the locator programmatically
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
     }
 }
